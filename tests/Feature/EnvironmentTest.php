677486dbaef29590cf1081210cdd1dc8c2d00436
Marleess Windows <marleysumarly@gmail.com>
2023-01-07 01:40:10 +0700

    menambah unit test

---
+++ tests/Feature/EnvironmentTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EnvironmentTest extends TestCase
+{
+    public function testGetEnv()
+    {
+        $result = env('COBA');
+
+        self::assertEquals('MARLEESS', $result);
+    }
+}
