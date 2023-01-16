3e7153cf795fc298f15e1e2154a5fd88afde9afc
Marleess Windows <marleysumarly@gmail.com>
2023-01-16 21:44:46 +0700

    Menambah named route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coba', function (){
+    return "Cobain";
+});
+
+Route::redirect('/cobain', '/coba');
+
+// costume fallback
+Route::fallback(function (){
+    return "Ga ada euy";
+});
+
+// get view
+Route::get('/hello', function (){
+   return view('user.hello', ['name' => 'Marleess']);
+});
+
+// regex routing
+Route::get('/products/{pId}', function ($pId){
+    return "Product $pId";
+})->name('product.detail');// named route
+
+// menentukan regex routing
+Route::get('/category/{cId}', function ($cId){
+    return "Category $cId";
+})->where('cId', '[0-9]+'); // named route
+
+// optional routing
+Route::get('user/{uId?}', function ($uId = "User facebook"){
+   return "User $uId";
+});
+
+Route::get('/produk/{id}', function ($produkId){
+    $link = route('product.detail', $produkId);
+    return "Link $link";
+});
+
+Route::get('/produk-redirect/{produkId}', function ($produkId){
+   return redirect()->route('product.detail', $produkId);
+});
